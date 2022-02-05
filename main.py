b3183626c2fa08d6e70abf091a384533e5daa56c
ʕ´•ᴥ•`ʔ╠ŞĦỮβĦa̷m̷╣ʕ´•ᴥ•`ʔ <63947251+shubhamg0sai@users.noreply.github.com>
2022-02-05 08:46:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,93 @@ import os
 import time
 from datetime import datetime
 import sys
-now = datetime.now()
-t=now.strftime('%I:%M:%p')
-print(t)
+from word2number import w2n
+from num2words import num2words
 
+now = datetime.now()
+nt1 = "speak Your first number"
+nt2 = "speak Your second number"
+op = "speak Your math operator like addition subtraction multiplication division"
+r = "the result is"
+t=now.strftime('%I:%M:%p')
+
+print(t)
 inp = subprocess.getoutput("termux-speech-to-text")
 time.sleep(1)
 print("......",str(inp))
+
+def add():
+    subprocess.call(["termux-tts-speak",nt1])
+    n1 = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(n1))
+    subprocess.call(["termux-tts-speak",nt2])
+    n2 = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(n2))
+    res = w2n.word_to_num(n1) + w2n.word_to_num(n2)
+    print(res)
+    result = num2words(res)
+    print(result)
+    subprocess.call(["termux-tts-speak",r,result])
+
+
+def sub():
+    subprocess.call(["termux-tts-speak",nt1])
+    n1 = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(n1))
+    subprocess.call(["termux-tts-speak",nt2])
+    n2 = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(n2))
+    res = w2n.word_to_num(n1) - w2n.word_to_num(n2)
+    print(res)
+    result = num2words(res)
+    print(result)
+    subprocess.call(["termux-tts-speak",r,result])
+
+
+def mul():
+    subprocess.call(["termux-tts-speak",nt1])
+    n1 = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(n1))
+    subprocess.call(["termux-tts-speak",nt2])
+    n2 = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(n2))
+    res = w2n.word_to_num(n1) * w2n.word_to_num(n2)
+    print(res)
+    result = num2words(res)
+    print(result)
+    subprocess.call(["termux-tts-speak",r,result])
+
+
+def div():
+    subprocess.call(["termux-tts-speak",nt1])
+    n1 = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(n1))
+    subprocess.call(["termux-tts-speak",nt2])
+    n2 = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(n2))
+    res = w2n.word_to_num(n1) / w2n.word_to_num(n2)
+    print(res)
+    result = num2words(res)
+    print(result)
+    subprocess.call(["termux-tts-speak",r,result])
+
+
+def calculator():
+    subprocess.call(["termux-tts-speak",op])
+    inp = subprocess.getoutput("termux-speech-to-text")
+    print("......",str(inp))
+    if "addition" in inp:      
+        add()
+    elif "substraction" in inp:       
+        sub()
+    elif "multiplication" in inp:      
+        mul()
+    elif "division" in inp:      
+        div()
+    else:
+        subprocess.call(["termux-tts-speak","wrong operator"])
+    calculator()
+
 
 def system():
      if inp == "":
@@ -91,7 +171,10 @@ def system():
      elif "video" in inp:
          os.system("termux-open https://www.google.com/search?q=video")
      
-     
+     elif "calculator" in inp:
+         calculator()
+
+
      
      
      
